97de4705834e6a4a61c80750ebe431d69c625c7b
Sparsh-Palkhiwala <palkhiwalasparsh2301@gmail.com>
2023-10-18 18:23:20 -0700

    572

--- CSE 571 - AI/Project 3/reinforcement/analysis.py
+++ CSE 571 - AI/Project 3/reinforcement/analysis.py
@@ -24,38 +24,38 @@ def question2():
     answerNoise = 0
     return answerDiscount, answerNoise
 
-def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3a():           # We want to reach the closest exit , so keep discounting low so as to take up the shortest path
+    answerDiscount = 0.1
+    answerNoise = 0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.2
+    answerNoise = 0.2
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3c():       #We want to reach the farther exit whilst risking the the cliff so we keep the noise zero and discount a little higher for it to explore more
+    answerDiscount = 0.6
+    answerNoise = 0
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3d():           #We prefer taking the safer route so we increase the noise ,and increase discount to promote exploration
+    answerDiscount = 0.6
+    answerNoise = 0.4
+    answerLivingReward = 0.2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+def question3e():           #We dont want it to exit or jump of the cliff, so we increase the discount and significantly increase the living reward so as to never move to exit
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = 11
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
